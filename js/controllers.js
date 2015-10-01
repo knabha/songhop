0f6c3bd9cbfa7c707fcaa32a8e60119bf2a1f78f
knabha <knabha.kn@gmail.com>
2015-10-01 22:50:32 +0000

    song hop files

---
+++ js/controllers.js
@@ -0,0 +1,56 @@
+angular.module('songhop.controllers', ['ionic', 'songhop.services'])
+
+.controller('DiscoverCtrl', function($scope,$timeout, User, Recommendations) {
+
+
+
+  // get our first songs
+  Recommendations.getNextSongs()
+    .then(function(){
+      $scope.currentSong = Recommendations.queue[0];
+    });
+
+ $scope.sendFeedback = function (bool) {
+
+    // first, add to favorites if they favorited
+    if (bool) User.addSongToFavorites($scope.currentSong);
+
+    // set variable for the correct animation sequence
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
+
+
+ // prepare the next song
+    Recommendations.nextSong();
+
+    $timeout(function() {
+      // $timeout to allow animation to complete
+      $scope.currentSong = Recommendations.queue[0];
+    }, 250);
+
+  }
+
+})
+
+/*
+Controller for the favorites page
+*/
+.controller('FavoritesCtrl', function($scope,User) {
+
+ // get the list of our favorites from the user service
+  $scope.favorites = User.favorites;
+
+
+  $scope.removeSong = function(song, index) {
+    User.removeSongFromFavorites(song, index);
+  }
+
+})
+
+
+/*
+Controller for our tab bar
+*/
+.controller('TabsCtrl', function($scope) {
+
+});
